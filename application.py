2084470e45e12a147b5e6fc389880f8979cf63d4
Halion <gaoyouhan@HalionMacBook-Pro.local>
2026-05-27 17:14:22 +0800

    feat: implement blog with github and google docs integration

--- application.py
+++ application.py
@@ -8,5 +8,11 @@ def home():
     google_doc_id = "1oupJh4l6Dlrun8gNbK0H83in7Kn6lhAnfxIMITR-PxU" 
     return render_template('blog.html', doc_id=google_doc_id)
 
+@application.route('/aws-guide')
+def aws_guide():
+    # 這是你提供的文件 ID
+    doc_id = "1oupJh4l6Dlrun8gNbK0H83in7Kn6lhAnfxIMITR-PxU"
+    return render_template('post.html', doc_id=doc_id)
+
 if __name__ == "__main__":
     application.run(host='0.0.0.0', port=8080)
