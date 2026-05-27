2cc9f4993800bbbfaf4bb07acac8ac1b6b9456a4
Halion <gaoyouhan@HalionMacBook-Pro.local>
2026-05-27 17:09:29 +0800

    feat: implement blog with github and google docs integration

---
+++ application.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+application = Flask(__name__)
+
+@application.route('/')
+def home():
+    # 這裡定義你的 Google 文件 ID
+    google_doc_id = "1oupJh4l6Dlrun8gNbK0H83in7Kn6lhAnfxIMITR-PxU" 
+    return render_template('blog.html', doc_id=google_doc_id)
+
+if __name__ == "__main__":
+    application.run(host='0.0.0.0', port=8080)
